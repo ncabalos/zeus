8eb99fa24c9289e972f08b00c57ac381c49a9c1a
Nathaniel Abalos <nathaniel_abalos@hotmail.com>
2013-11-17 09:57:02 -0600

    Added empty function for hardware initialization

--- source/main.c
+++ source/main.c
@@ -9,23 +9,34 @@
 #include <stdlib.h>
 
 #include <p24Fxxxx.h>
-#include <FreeRTOS.h>
 
 #ifdef UNIT_TEST
 #include <unity.h>
 #include <unit_tests.h>
+#else
+#include <FreeRTOS.h>
+#include <task.h>
 #endif
 
-/*
- * 
- */
-#ifndef UNIT_TEST
-int main(int argc, char** argv) {
+/* Function Protoypes *********************************************************/
+static void HardwareInit(void);
 
+#ifndef UNIT_TEST
+
+void HardwareInit(void)
+{
+}
+
+int main(int argc, char** argv)
+{
+    HardwareInit();
+    vTaskStartScheduler();
     return (EXIT_SUCCESS);
 }
 #else
-int main(int argc, char** argv) {
+
+int main(int argc, char** argv)
+{
     UnityBegin();
     UnityEnd();
     return (EXIT_SUCCESS);
